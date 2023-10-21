a2be4b01bc95893d8871effe3c18bcf0b00c0a84
HanLeqi0117 <k895657@kansai-u.ac.jp>
2023-10-21 12:45:04 +0900

    add new sources

---
+++ scripts/navsat_fix_extraction.py
@@ -0,0 +1,113 @@
+#! /usr/bin/python3
+
+import os, sys, csv, enum, datetime
+import rospy, roslib.packages
+from sensor_msgs.msg import NavSatFix, NavSatStatus
+
+fix_topic = rospy.get_param("fix_topic", default="fix")
+fix_filtered_topic = rospy.get_param("fix_filtered_topic", default="gps/filtered")
+file_save_path = rospy.get_param("file_save_path", default=os.path.join(roslib.packages.get_pkg_dir(), "doc"))
+csv_header_list = ["Topic", "Sequence", "FrameID", "RosStamp", "Latitude", "Longitude", "Altitude", "NavSatStatus", "SatelliteSystem", "PositionCovarianceType", "PositionCovariance"]
+fix_seq = 1
+fix_filtered_seq = 1
+fix_matrix = fix_filtered_matrix = [[]]
+
+class Status(enum.Enum):
+    STATUS_NO_FIX = NavSatStatus.STATUS_NO_FIX
+    STATUS_FIX = NavSatStatus.STATUS_FIX
+    STATUS_SBAS_FIX = NavSatStatus.STATUS_SBAS_FIX
+    STATUS_GBAS_FIX = NavSatStatus.STATUS_GBAS_FIX
+
+class Service(enum.Enum):
+    SERVICE_GPS = NavSatStatus.SERVICE_GPS
+    SERVICE_GLONASS = NavSatStatus.SERVICE_GLONASS
+    SERVICE_COMPASS = NavSatStatus.SERVICE_COMPASS
+    SERVICE_GALILEO = NavSatStatus.SERVICE_GALILEO
+
+class PositionCovarianceType(enum.Enum):
+    COVARIANCE_TYPE_UNKNOWN = NavSatFix.COVARIANCE_TYPE_UNKNOWN
+    COVARIANCE_TYPE_APPROXIMATED = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+    COVARIANCE_TYPE_DIAGONAL_KNOWN = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
+    COVARIANCE_TYPE_KNOWN = NavSatFix.COVARIANCE_TYPE_KNOWN
+
+def fix_callback(msg = NavSatFix):
+    for item in Status:
+        if msg.status.status == item.value:
+            navsat_status = item.name
+    for item in Service:
+        if msg.status.status == item.value:
+            navsat_service = item.name
+    for item in PositionCovarianceType:
+        if msg.position_covariance_type == item.value:
+            pos_cov_type = item.name
+    
+    pos_cov_list = [str(item) for item in msg.position_covariance] 
+    pos_cov_row1 = ' ,'.join(pos_cov_list[0:3])
+    pos_cov_row2 = ' ,'.join(pos_cov_list[3:6])
+    pos_cov_row3 = ' ,'.join(pos_cov_list[6:])
+    pos_cov_string = '[[{}], [{}], [{}]]'.format(pos_cov_row1, pos_cov_row2, pos_cov_row3)
+    
+    fix_matrix.append([
+        fix_topic, fix_seq, msg.header.frame_id, msg.header.stamp.to_sec(), 
+        msg.latitude, msg.longitude, msg.altitude, 
+        navsat_status, navsat_service, pos_cov_type, pos_cov_string
+    ])
+    
+    fix_seq =+ 1
+    
+
+def filtered_fix_callback(msg = NavSatFix):
+    for item in Status:
+        if msg.status.status == item.value:
+            navsat_status = item.name
+    for item in Service:
+        if msg.status.status == item.value:
+            navsat_service = item.name
+    for item in PositionCovarianceType:
+        if msg.position_covariance_type == item.value:
+            pos_cov_type = item.name
+    
+    pos_cov_list = [str(item) for item in msg.position_covariance] 
+    pos_cov_row1 = ' ,'.join(pos_cov_list[0:3])
+    pos_cov_row2 = ' ,'.join(pos_cov_list[3:6])
+    pos_cov_row3 = ' ,'.join(pos_cov_list[6:])
+    pos_cov_string = '[[{}], [{}], [{}]]'.format(pos_cov_row1, pos_cov_row2, pos_cov_row3)
+    
+    fix_matrix.append([
+        fix_topic, fix_filtered_seq, msg.header.frame_id, msg.header.stamp.to_sec(), 
+        msg.latitude, msg.longitude, msg.altitude, 
+        navsat_status, navsat_service, pos_cov_type, pos_cov_string
+    ])
+    
+    fix_filtered_seq =+ 1
+
+    
+def shutdown_callback():
+    
+    csv_file_name = fix_topic + "_{:%Y_%m_%d_%H_%M_%S}.csv".format(datetime.datetime.now())
+    rospy.loginfo("NavSatFix Data is saved in [{}/{}]".format(file_save_path, csv_file_name))
+
+    # Write Fix
+    with open(os.path.join(file_save_path, csv_file_name), "w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(csv_header_list)
+        for item in fix_matrix:
+            csv_writer.writerow(item)
+
+    csv_file_name = fix_filtered_topic + "_{:%Y_%m_%d_%H_%M_%S}.csv".format(datetime.datetime.now())
+    rospy.loginfo("NavSatFixFiltered Data is saved in [{}/{}]".format(file_save_path, csv_file_name))
+    
+    # Write Fix Filtered
+    with open(os.path.join(file_save_path, csv_file_name), "w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(csv_header_list)
+        for item in fix_filtered_matrix:
+            csv_writer.writerow(item)
+            
+if __name__ == '__main__':
+    
+    rospy.init_node(name="navsat_fix_extraction")
+    rospy.Subscriber(fix_topic, NavSatFix, fix_callback, queue_size=30)
+    rospy.Subscriber(fix_filtered_topic, NavSatFix, filtered_fix_callback, queue_size=30)
+    rospy.loginfo("Ready to receive NavsatFix and NavsatFixFiltered from Topic.")
+    rospy.on_shutdown(shutdown_callback)
--- scripts/nmea_extraction.py
+++ scripts/nmea_extraction.py
@@ -4,16 +4,17 @@ import os
 import sys
 import csv
 import rospy
+import roslib.packages
+from datetime import datetime
 from nmea_msgs.msg import Sentence
 
-change_file_flag = False
-directory_path = str()
-csv_file_name = str()
+file_save_path = rospy.get_param("file_save_path", default=os.path.join(roslib.packages.get_pkg_dir(), "doc"))
+recieve_topic_name = rospy.get_param("recieve_topic_name", default="nmea_sentence")
 nmea_index_list = ["GGA", "GSV", "GSA", "GLL", "ZDA", "VTG", "RMC"]
 nmea_dict = dict()
 for nmea_index in nmea_index_list:
     nmea_dict[nmea_index] = [[]]
-ros_message_header_list = ["Sequence", "FrameID", "RosSec", "RosNsec"]
+ros_message_header_list = ["Sequence", "FrameID", "RosStamp"]
 
 # 参考URL:https://www.hdlc.jp/~jh8xvh/jp/gps/nmea.html
 # 参考URL:https://jp.mathworks.com/help/nav/ref/nmeaparser-system-object.html
@@ -75,8 +76,7 @@ def nmea_callback(msg = Sentence):
             # Header of ROS Message
             list_tmp.append(msg.header.seq)
             list_tmp.append(msg.header.frame_id)
-            list_tmp.append(msg.header.stamp.secs)
-            list_tmp.append(msg.header.stamp.nsecs)
+            list_tmp.append(msg.header.stamp.to_sec())
             # Sentence String -> List
             for detail in msg.sentence[:-3].split(","):
                 if "$" in detail:
@@ -92,133 +92,103 @@ def nmea_callback(msg = Sentence):
             
 def shutdown_callback():
     # debug 
-    # print(nmea_dict)    
-    if change_file_flag:
-        rospy.loginfo("NmeaSentence Data is saved in [{}/{}]".format(directory_path, csv_file_name))
+    # print(nmea_dict)
+    
+    csv_file_name = recieve_topic_name + "_{:%Y_%m_%d_%H_%M_%S}.csv".format(datetime.now())
+    rospy.loginfo("NmeaSentence Data is saved in [{}/{}]".format(file_save_path, csv_file_name))
         
-        with open(os.path.join(directory_path, csv_file_name), "w") as f:
-            for nmea_index in nmea_index_list:
-                if len(nmea_dict[nmea_index]) == 1 and len(nmea_dict[nmea_index][0]) == 0:
-                    continue
+    with open(os.path.join(file_save_path, csv_file_name), "w") as f:
+        for nmea_index in nmea_index_list:
+            if len(nmea_dict[nmea_index]) == 1 and len(nmea_dict[nmea_index][0]) == 0:
+                continue
+            
+            if "GSA" in nmea_index:
+                # Write Header
+                csv_writer = csv.writer(f)
+                first_row_list = ["NmeaType"]
+                first_row_list.extend(ros_message_header_list)
+                for index in header_dict[nmea_index]:
+                    if "SatellitesIDNumber" in index:
+                        first_row_list.append('SatellitesIDNumber')
+                        for i in range(11):
+                            first_row_list.append('')
+                    else:
+                        first_row_list.append(index)
+                csv_writer.writerow(first_row_list)
+            else:
+                # Write Header
+                csv_writer = csv.writer(f)
+                first_row_list = ["NmeaType"]
+                first_row_list.extend(ros_message_header_list)
+                first_row_list.extend(header_dict[nmea_index])
+                csv_writer.writerow(first_row_list)
                 
-                if "GSA" in nmea_index:
-                    # Write Header
-                    csv_writer = csv.writer(f)
-                    first_row_list = ["NmeaType"]
-                    first_row_list.extend(ros_message_header_list)
-                    for index in header_dict[nmea_index]:
-                        if "SatellitesIDNumber" in index:
-                            first_row_list.append('SatellitesIDNumber')
-                            for i in range(11):
-                                first_row_list.append('')
-                        else:
-                            first_row_list.append(index)
-                    csv_writer.writerow(first_row_list)
-                else:
-                    # Write Header
-                    csv_writer = csv.writer(f)
-                    first_row_list = ["NmeaType"]
-                    first_row_list.extend(ros_message_header_list)
-                    first_row_list.extend(header_dict[nmea_index])
-                    csv_writer.writerow(first_row_list)
-                    
-                if "GSV" in nmea_index:
-                    for nmea_dict_data_row in nmea_dict[nmea_index]:                
-                        if len(nmea_dict_data_row) == 18:
-                            header = nmea_dict_data_row[:8]
-                            row1 = nmea_dict_data_row[9:12]
-                            row2 = nmea_dict_data_row[13:16]
-                            tail = nmea_dict_data_row[-1:]
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row1)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row2)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
-                            
-                        elif len(nmea_dict_data_row) == 26:
-                            header = nmea_dict_data_row[:8]
-                            row1 = nmea_dict_data_row[9:12]
-                            row2 = nmea_dict_data_row[13:16]
-                            row3 = nmea_dict_data_row[17:20]
-                            row4 = nmea_dict_data_row[21:24]
-                            tail = nmea_dict_data_row[-1:]
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row1)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row2)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row3)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
-                            
-                            row_tmp = [nmea_index]
-                            row_tmp.extend(header)
-                            row_tmp.extend(row4)
-                            row_tmp.extend(tail)
-                            csv_writer.writerow(row_tmp)
+            if "GSV" in nmea_index:
+                for nmea_dict_data_row in nmea_dict[nmea_index]:                
+                    if len(nmea_dict_data_row) == 17:
+                        header = nmea_dict_data_row[:9]
+                        row1 = nmea_dict_data_row[9:13]
+                        row2 = nmea_dict_data_row[13:17]
+                        tail = nmea_dict_data_row[-1:]
                         
-                                        
-                else:
-                    # Write Detail
-                    for nmea_dict_data_row in nmea_dict[nmea_index]:
-                        if len(nmea_dict_data_row) == 0:
-                            continue
                         row_tmp = [nmea_index]
-                        row_tmp.extend(nmea_dict_data_row)
+                        row_tmp.extend(header)
+                        row_tmp.extend(row1)
+                        row_tmp.extend(tail)
                         csv_writer.writerow(row_tmp)
                         
-    else:
-        rospy.loginfo("No change happened.")
+                        row_tmp = [nmea_index]
+                        row_tmp.extend(header)
+                        row_tmp.extend(row2)
+                        row_tmp.extend(tail)
+                        csv_writer.writerow(row_tmp)
+                        
+                    elif len(nmea_dict_data_row) == 25:
+                        header = nmea_dict_data_row[:9]
+                        row1 = nmea_dict_data_row[9:13]
+                        row2 = nmea_dict_data_row[13:17]
+                        row3 = nmea_dict_data_row[17:21]
+                        row4 = nmea_dict_data_row[21:25]
+                        tail = nmea_dict_data_row[-1:]
+                        
+                        row_tmp = [nmea_index]
+                        row_tmp.extend(header)
+                        row_tmp.extend(row1)
+                        row_tmp.extend(tail)
+                        csv_writer.writerow(row_tmp)
+                        
+                        row_tmp = [nmea_index]
+                        row_tmp.extend(header)
+                        row_tmp.extend(row2)
+                        row_tmp.extend(tail)
+                        csv_writer.writerow(row_tmp)
+                        
+                        row_tmp = [nmea_index]
+                        row_tmp.extend(header)
+                        row_tmp.extend(row3)
+                        row_tmp.extend(tail)
+                        csv_writer.writerow(row_tmp)
+                        
+                        row_tmp = [nmea_index]
+                        row_tmp.extend(header)
+                        row_tmp.extend(row4)
+                        row_tmp.extend(tail)
+                        csv_writer.writerow(row_tmp)
+                                
+            else:
+                # Write Detail
+                for nmea_dict_data_row in nmea_dict[nmea_index]:
+                    if len(nmea_dict_data_row) == 0:
+                        continue
+                    row_tmp = [nmea_index]
+                    row_tmp.extend(nmea_dict_data_row)
+                    csv_writer.writerow(row_tmp)
+
             
 if __name__ == '__main__':
     
     rospy.init_node(name="nmea_extraction")
-    if len(sys.argv) < 4:
-        rospy.loginfo("usage: [-d Path to save csv] [-f CSV file name]")
-        sys.exit()
-    
-    if '-d' in sys.argv:
-        directory_path = sys.argv[sys.argv.index('-d') + 1]
-        if not (os.path.exists(directory_path) or os.path.isdir(directory_path)):
-            rospy.logwarn("Directory given is not existed!")
-            sys.exit()
-    else:
-        rospy.loginfo("-d option need!")
-    
-    if '-f' in sys.argv:
-        csv_file_name = sys.argv[sys.argv.index('-f') + 1]
-        if csv_file_name in os.listdir(directory_path):
-            if not ".csv" in csv_file_name:
-                csv_file_name = csv_file_name + ".csv"
-            rospy.logwarn("File name given is existed: {}".format(csv_file_name))
-            sys.exit()
-        elif csv_file_name == '':
-            csv_file_name = "nmea_sentence_table.csv"
-            rospy.logwarn("File name is empty, change it to default: {}".format(csv_file_name))
-            sys.exit()
-    else:
-        rospy.loginfo("-f option need!")
-        sys.exit()
-
-    change_file_flag = True
-    rospy.Subscriber("nmea_sentence", Sentence, nmea_callback, queue_size=30)
+    rospy.Subscriber(recieve_topic_name, Sentence, nmea_callback, queue_size=30)
     rospy.loginfo("Ready to receive NmeaSentence from Topic.")
     rospy.spin()
     rospy.on_shutdown(shutdown_callback)
